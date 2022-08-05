535516bc9eedb586f591f177f713a8431b16ff4d
ujjal <ruet.ujjal38@gmail.com>
2022-08-05 14:03:31 +0600

    Blocked while converting a flux to stream

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise3 {
 
@@ -10,11 +12,13 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
 
+        //Blocking code
+        List<Integer> numbers = ReactiveSources.intNumbersFlux().toStream().collect(Collectors.toList());
 
-        System.out.println("Press a key to end");
-        System.in.read();
+        //after all the items are collected,it will take 9-10 seconds,then only below code will be executed
+        System.out.println("List is " + numbers);
+        System.out.println("Size is : " + numbers.size());
     }
 
 }
